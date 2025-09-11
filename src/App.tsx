767ba7a4c20191f641af63676fae1267800ee9a5
Jake Lohman <jakelohman7@gmail.com>
2025-09-11 10:46:55 -0500

    Header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import "./App.css";
 import Bio from "./components/Bio";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Highlights from "./components/Highlights";
 
 export default function App() {
   return (
     <>
-      <h1>Min Kyung Lee</h1>
-      <Bio />
-      <hr />
-      <Highlights />
+      <Header />
+
+      <div id="content">
+        <h1>Min Kyung Lee</h1>
+        <Bio />
+        <hr />
+        <Highlights />
+      </div>
+
+      <Footer />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div
+      style={{ backgroundColor: "#f1f1f1", width: "100%", height: "44px" }}
+    />
+  );
+}
