b915c6bd106abb7cad5483e3e4db058497e22b37
Jake Lohman <jakelohman7@gmail.com>
2025-09-16 10:18:27 -0500

    Selected highlight styling prototype

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -5,11 +5,14 @@ export default function Highlights() {
   return (
     <div>
       <h2 id="highlight-title">SELECTED HIGHLIGHTS</h2>
-      {highlights.map((highlight, idx) => (
-        <div key={`highlight_${idx}`} className="highlight">
-          {highlight}
-        </div>
-      ))}
+
+      <div id="highlights-group">
+        {highlights.map((highlight, idx) => (
+          <div key={`highlight_${idx}`} className="highlight">
+            {highlight}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
